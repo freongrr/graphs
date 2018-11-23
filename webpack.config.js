a5cb01a209c18ae7b59cfe69e25c61e7f949487d
Fabien Cortina <fabien.cortina@gmail.com>
2018-11-23 12:28:35 +0900

    Webify

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const APP_DIR = path.resolve(__dirname, "src/");
+const BUILD_DIR = path.resolve(__dirname, "dist/");
+
+module.exports = {
+    entry: APP_DIR + "/index.js",
+    output: {
+        path: BUILD_DIR,
+        filename: "[name].bundle.js"
+    },
+    resolve: {
+        alias: {
+            "vue$": "vue/dist/vue.esm.js"
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: [
+                "source-map-loader",
+            ],
+            enforce: "pre"
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+                "babel-loader",
+                "eslint-loader"
+            ]
+        }, {
+            test: /\.scss$/,
+            use: [
+                "style-loader?sourceMap",
+                "css-loader?sourceMap",
+                "sass-loader?sourceMap"
+            ]
+        }]
+    },
+    devtool: "source-maps",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: APP_DIR + "/index.html"
+        })
+    ]
+};
